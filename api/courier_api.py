9c75057ba9d130ad20ef67af53b88229284e2cc2
Maksim Perunkov <nanellod@yandex.ru>
2025-12-19 13:08:08 +0300

    add anotation allure.step for logic methods

--- api/courier_api.py
+++ api/courier_api.py
@@ -1,16 +1,20 @@
 import requests
+import allure
 from urls import Urls
 
 class CourierApi:
     @staticmethod
+    @allure.step("Создание курьера")
     def create_courier(body):
         return requests.post(Urls.CREATE_COURIER_URL, json=body)
     
     @staticmethod
+    @allure.step("Авторизация курьера")
     def login_courier(body):
         return requests.post(Urls.LOGIN_COURIER_URL, json=body)
     
     @staticmethod
+    @allure.step("Удаление курьера")
     def delete_courier(courier_id):
         return requests.delete(Urls.DELETE_COURIER_URL.format(courier_id=courier_id))
     
